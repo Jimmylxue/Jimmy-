17ba22492e2167beb3c290bbf317ae1fa7739ada
Jimmy <1002661758@qq.com>
2020-08-14 17:23:08 +0800

    change

--- start.sh
+++ start.sh
@@ -1,11 +1,14 @@
+# 拉去代码
 git pull
 
+# 安装依赖
 cnpm install && cnpm run build
 
+# 删除容器
 docker rm -f myBlog &> /dev/null
 
+# 重启容器
 docker run -d --restart=on-failure:5 \
     -p 80:80 \
-    -v $PWD/public:user/share/nginx/html \
+    -v $PWD/public:usr/share/nginx/html \
     --name myBlog nginx
-
