cdb7b7218d8e91d105412cd9df35767181db7b75
jimmyxuexue <1002661758@qq.com>
2021-07-11 20:08:22 +0800

    update

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 # 拉去代码
 git pull
 # 安装依赖
-cnpm install && cnpm run build
+npm install && npm run build
 
 # 删除容器
 docker rm -f myblog &> /dev/null
 
 # 重启容器
 docker run -d --restart=on-failure:5\
-    -p 80:80 \
+    -p 666:80 \
     -v $PWD/public:/usr/share/nginx/html \
     --name myblog nginx
